a10e82a4ef4e862f8649119e08d2db915c109e7a
Oguz Bastemur <obastemur@gmail.com>
2015-06-15 18:55:23 +0200

    add JXcore interface header

---
+++ src/leveldown_public.h
@@ -0,0 +1,8 @@
+// Copyright JXcore Contributors - MIT
+// JXcore uses this file to embed Leveldown
+
+#include <node.h>
+
+namespace leveldown {
+  extern void RegisterModule(JS_HANDLE_OBJECT_REF target);
+}
