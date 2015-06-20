bf1290369dc01c45d75dc4f2b4aa0819d884551f
Oguz Bastemur <obastemur@gmail.com>
2015-06-20 21:12:43 +0200

    Update changelog + cleanup

--- leveldown.js
+++ leveldown.js
@@ -4,9 +4,9 @@ const util              = require('util')
 var binding;
 
 if (process.versions.embedded && process.versions.embedded.leveldown)
-	binding = jxcore.embeddedModule.require('leveldown').leveldown;
+  binding = jxcore.embeddedModule.require('leveldown').leveldown;
 else
-	binding = require('bindings')('leveldown.node').leveldown;
+  binding = require('bindings')('leveldown.node').leveldown;
     
 var ChainedBatch      = require('./chained-batch')
     , Iterator        = require('./iterator')
